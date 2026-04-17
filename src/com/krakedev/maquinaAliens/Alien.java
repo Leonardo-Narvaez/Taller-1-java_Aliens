219e7d50345b099cf65ccc9985eb2c8427d2842c
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2026-04-16 21:06:58 -0500

    LNMQ: creacion de clase y constructor

---
+++ src/com/krakedev/maquinaAliens/Alien.java
@@ -0,0 +1,26 @@
+package com.krakedev.maquinaAliens;
+
+public class Alien {
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjo;
+	private double precioCuerpo;
+
+	public void Alien(int tamanio, String color) {
+		if (tamanio < 5) {
+			this.tamanio = 5;
+		} else if (tamanio > 30) {
+			this.tamanio = 30;
+		} else {
+			this.tamanio = tamanio;
+		}
+
+		this.precioCuerpo = 0.2 * this.tamanio;
+		this.precioExtremidad = 0.1 * this.tamanio;
+		this.precioOjo = 0.05 * tamanio;
+	}
+}
