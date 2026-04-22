f1b70be1d69a8bf1901afb0bdb41b227a47251e0
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2026-04-21 23:51:51 -0500

    LNMQ: se agrega pruebas jUnit

--- src/com/krakedev/maquinaAliens/Alien.java
+++ src/com/krakedev/maquinaAliens/Alien.java
@@ -10,7 +10,39 @@ public class Alien {
 	private double precioOjo;
 	private double precioCuerpo;
 
-	public void Alien(int tamanio, String color) {
+	public int getTamanio() {
+		return tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getNumeroOjos() {
+		return numeroOjos;
+	}
+
+	public int getNumeroBrazos() {
+		return numeroBrazos;
+	}
+
+	public int getNumeroPies() {
+		return numeroPies;
+	}
+
+	public double getPrecioExtremidad() {
+		return precioExtremidad;
+	}
+
+	public double getPrecioOjo() {
+		return precioOjo;
+	}
+
+	public double getPrecioCuerpo() {
+		return precioCuerpo;
+	}
+
+	public Alien(int tamanio, String color) {
 		if (tamanio < 5) {
 			this.tamanio = 5;
 		} else if (tamanio > 30) {
@@ -18,9 +50,21 @@ public class Alien {
 		} else {
 			this.tamanio = tamanio;
 		}
-
+		this.color = color;
 		this.precioCuerpo = 0.2 * this.tamanio;
 		this.precioExtremidad = 0.1 * this.tamanio;
 		this.precioOjo = 0.05 * tamanio;
+	}
+
+	public void imprimir() {
+		System.out.println("Tamaño: " + tamanio);
+		System.out.println("Color: " + color);
+		System.out.println("Numero de ojos: " + numeroOjos);
+		System.out.println("Numero de brazos: " + numeroBrazos);
+		System.out.println("Numero pies: " + numeroPies);
+		System.out.println("Precio de extremidad: " + precioExtremidad);
+		System.out.println("Precio de ojo: " + precioOjo);
+		System.out.println("Precio de cuerpo: " + precioCuerpo);
+
 	}
 }
---
+++ src/com/krakedev/maquinaAliens/test/TestContructores.java
@@ -0,0 +1,16 @@
+package com.krakedev.maquinaAliens.test;
+
+import com.krakedev.maquinaAliens.Alien;
+
+public class TestContructores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Alien alien1 = new Alien(4, "verde");
+		Alien alien2 = new Alien(26, "negro");
+
+		alien1.imprimir();
+		alien2.imprimir();
+	}
+
+}
